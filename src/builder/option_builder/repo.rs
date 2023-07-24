922528cb508799491793ed66262edbbf6efb7f16
nogtk <taka.naoga@moneyforward.co.jp>
2023-07-24 21:27:12 +0900

    Enable to search all repository

--- src/builder/option_builder/repo.rs
+++ src/builder/option_builder/repo.rs
@@ -3,10 +3,19 @@ use inquire::Text;
 
 pub fn build() -> GhOption {
     let t = Text::new("Which repo do you wanna search?")
+        .with_help_message("To search all repo, just enter with nothing")
         .prompt()
         .unwrap();
-    GhOption {
-        arg: Some(String::from("-R")),
-        content: Some(t),
+
+    if t.is_empty() {
+        GhOption {
+            arg: None,
+            content: None,
+        }
+    } else {
+        GhOption {
+            arg: Some(String::from("-R")),
+            content: Some(t),
+        }
     }
 }
