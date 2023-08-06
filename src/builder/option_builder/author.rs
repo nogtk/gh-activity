67846a1b7e3f15141218325d8bc99c90506ef5f1
nogtk <taka.naoga@moneyforward.co.jp>
2023-08-06 15:18:40 +0900

    Selectable repo authors

--- src/builder/option_builder/author.rs
+++ src/builder/option_builder/author.rs
@@ -1,13 +1,31 @@
 use super::GhOption;
-use inquire::Text;
+use inquire::{Confirm, Text};
 
 pub fn build() -> GhOption {
-    let t = Text::new("Who is the author?")
-        .with_default("@me")
-        .prompt()
-        .unwrap();
-    GhOption {
-        arg: Some(String::from("--author")),
-        content: Some(t),
+    let c = Confirm::new("Do you specify the author?")
+        .with_default(false)
+        .prompt();
+
+    match c {
+        Ok(true) => {
+            let t = Text::new("Who is the author?")
+                .with_default("@me")
+                .with_help_message("type @me to search for your own repos")
+                .prompt()
+                .unwrap();
+            GhOption {
+                arg: Some(String::from("--author")),
+                content: Some(t),
+            }
+        }
+        Ok(false) => {
+            return GhOption {
+                arg: None,
+                content: None,
+            }
+        }
+        Err(_) => {
+            panic!("occur some errors")
+        }
     }
 }
